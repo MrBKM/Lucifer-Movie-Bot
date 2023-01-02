e530f50f163f8ea85173e880160b79f97cc3d8c5
BKM TG <119480777+MrBKM@users.noreply.github.com>
2023-01-02 23:37:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot information
 PORT = environ.get("PORT", "8080")
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '25085908'))
-API_HASH = environ('API_HASH', '86b8a77b1ecb0507a3da3934e6a2c184')
-BOT_TOKEN = environ('BOT_TOKEN', '5979444624:AAEl8TcjEtaw3sxdPSsOHehJZPzFXrfcw5A')
+API_ID = int(environ.get('API_ID', '25085908'))
+API_HASH = environ.get('API_HASH', '86b8a77b1ecb0507a3da3934e6a2c184')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5979444624:AAEl8TcjEtaw3sxdPSsOHehJZPzFXrfcw5A')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
